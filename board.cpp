14c57eaf72774c11efafac3670c511d879b36dca
kouroshshahidi <kourosh.a.shahidi@gmail.com>
2024-02-05 16:37:53 +0330

    new header file : save and load

--- board.cpp
+++ board.cpp
@@ -7,6 +7,7 @@
 #include "player.h"
 #include "move_system.h"
 #include "dfs.h"
+#include "save.h"
 
 //data storage
 //struct player{
@@ -410,45 +411,45 @@ int printBoard(int arr[][50], int n) {
 //    }
 //}
 
-void saveGame (int board[][50], int &n, player &p1, player &p2, int &decision, int &turn) {
-    FILE *inputFile1, *inputFile2;
-    inputFile1 = fopen("E:\\corridorProject\\save\\board.dat", "wb");
-    for (int i = 0; i < 50; ++i) {
-        fwrite(board[i], sizeof(int), 50, inputFile1);
-    }
-    fwrite(&n, sizeof(int), 1, inputFile1);
-    fwrite(&decision, sizeof(int), 1, inputFile1);
-    fwrite(&turn, sizeof(int), 1, inputFile1);
+//void saveGame (int board[][50], int &n, player &p1, player &p2, int &decision, int &turn) {
+//    FILE *inputFile1, *inputFile2;
+//    inputFile1 = fopen("E:\\project\\quoridor\\save\\board.dat", "wb");
+//    for (int i = 0; i < 50; ++i) {
+//        fwrite(board[i], sizeof(int), 50, inputFile1);
+//    }
+//    fwrite(&n, sizeof(int), 1, inputFile1);
+//    fwrite(&decision, sizeof(int), 1, inputFile1);
+//    fwrite(&turn, sizeof(int), 1, inputFile1);
+//
+//    inputFile2 = fopen("E:\\project\\quoridor\\save\\player.dat", "wb");
+//    fwrite(&p1, sizeof(player), 1, inputFile2);
+//    fwrite(&p2, sizeof(player), 1, inputFile2);
+//    fclose(inputFile1);
+//    fclose(inputFile2);
+//}
 
-    inputFile2 = fopen("E:\\corridorProject\\save\\player.dat", "wb");
-    fwrite(&p1, sizeof(player), 1, inputFile2);
-    fwrite(&p2, sizeof(player), 1, inputFile2);
-    fclose(inputFile1);
-    fclose(inputFile2);
-}
-
-void loadGame (int board[][50], int &n, player &p1, player &p2, int &decision, int &turn) {
-    FILE *inputFile1, *inputFile2;
-    inputFile1 = fopen("E:\\corridorProject\\save\\board.dat", "rb");
-    for (int i = 0; i < 50; ++i) {
-        fread(board[i], sizeof(int), 50, inputFile1);
-    }
-    fread(&n, sizeof(int), 1, inputFile1);
-    fread(&decision, sizeof(int), 1, inputFile1);
-    fread(&turn, sizeof(int), 1, inputFile1);
-
-    inputFile2 = fopen("E:\\corridorProject\\save\\player.dat", "rb");
-    fread(&p1, sizeof(player), 1, inputFile2);
-    fread(&p2, sizeof(player), 1, inputFile2);
-    fclose(inputFile1);
-    fclose(inputFile2);
-
-    // show information
-    printf("%s have %d walls\n", p1.name, p1.wallNumber);
-    printf("%s have %d walls\n", p2.name, p2.wallNumber);
-    printBoard(board, 2*n +1);
-
-}
+//void loadGame (int board[][50], int &n, player &p1, player &p2, int &decision, int &turn) {
+//    FILE *inputFile1, *inputFile2;
+//    inputFile1 = fopen("E:\\project\\quoridor\\save\\board.dat", "rb");
+//    for (int i = 0; i < 50; ++i) {
+//        fread(board[i], sizeof(int), 50, inputFile1);
+//    }
+//    fread(&n, sizeof(int), 1, inputFile1);
+//    fread(&decision, sizeof(int), 1, inputFile1);
+//    fread(&turn, sizeof(int), 1, inputFile1);
+//
+//    inputFile2 = fopen("E:\\project\\quoridor\\save\\player.dat", "rb");
+//    fread(&p1, sizeof(player), 1, inputFile2);
+//    fread(&p2, sizeof(player), 1, inputFile2);
+//    fclose(inputFile1);
+//    fclose(inputFile2);
+//
+//    // show information
+//    printf("%s have %d walls\n", p1.name, p1.wallNumber);
+//    printf("%s have %d walls\n", p2.name, p2.wallNumber);
+//    printBoard(board, 2*n +1);
+//
+//}
 
 int main(void) {
     srand (time(NULL));
@@ -531,7 +532,8 @@ int main(void) {
 
 //move system
     if (swLoad == 1) turn = 2;
-    if (swLoad == 1) printBoard(board , 2*n +1);
+    printf("\n");
+    printBoard(board , 2*n +1);
     printf("\n");
 
     while(p2.y != 0 && p1.y != n-1){
---
+++ save.h
@@ -0,0 +1,47 @@
+#ifndef QUORIDOR_SAVE_H
+#define QUORIDOR_SAVE_H
+
+#include <stdio.h>
+#include "player.h"
+
+void saveGame (int board[][50], int &n, player &p1, player &p2, int &decision, int &turn) {
+    FILE *inputFile1, *inputFile2;
+    inputFile1 = fopen("E:\\project\\quoridor\\save\\board.dat", "wb");
+    for (int i = 0; i < 50; ++i) {
+        fwrite(board[i], sizeof(int), 50, inputFile1);
+    }
+    fwrite(&n, sizeof(int), 1, inputFile1);
+    fwrite(&decision, sizeof(int), 1, inputFile1);
+    fwrite(&turn, sizeof(int), 1, inputFile1);
+
+    inputFile2 = fopen("E:\\project\\quoridor\\save\\player.dat", "wb");
+    fwrite(&p1, sizeof(player), 1, inputFile2);
+    fwrite(&p2, sizeof(player), 1, inputFile2);
+    fclose(inputFile1);
+    fclose(inputFile2);
+}
+
+void loadGame (int board[][50], int &n, player &p1, player &p2, int &decision, int &turn) {
+    FILE *inputFile1, *inputFile2;
+    inputFile1 = fopen("E:\\project\\quoridor\\save\\board.dat", "rb");
+    for (int i = 0; i < 50; ++i) {
+        fread(board[i], sizeof(int), 50, inputFile1);
+    }
+    fread(&n, sizeof(int), 1, inputFile1);
+    fread(&decision, sizeof(int), 1, inputFile1);
+    fread(&turn, sizeof(int), 1, inputFile1);
+
+    inputFile2 = fopen("E:\\project\\quoridor\\save\\player.dat", "rb");
+    fread(&p1, sizeof(player), 1, inputFile2);
+    fread(&p2, sizeof(player), 1, inputFile2);
+    fclose(inputFile1);
+    fclose(inputFile2);
+
+    // show information
+    printf("%s have %d walls\n", p1.name, p1.wallNumber);
+    printf("%s have %d walls\n", p2.name, p2.wallNumber);
+
+}
+
+
+#endif //QUORIDOR_SAVE_H
